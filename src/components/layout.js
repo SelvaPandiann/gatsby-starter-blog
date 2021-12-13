8e6f99af688e97440c7e024da83ab3ed6f17c197
SelvaPandiann <96069854+SelvaPandiann@users.noreply.github.com>
2021-12-13 21:28:50 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-const Layout = ({ location, children }) => {
+const Layout = ({ location,title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
@@ -9,7 +9,7 @@ const Layout = ({ location, children }) => {
   if (isRootPath) {
     header = (
       <h1 className="main-heading">
-        <Link to="/">Selvapandian's Blog</Link>
+        <Link to="/">{title}</Link>
       </h1>
     )
   } else {
